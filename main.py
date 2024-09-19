8305f16eaeb9e659f49f0cee3572fe7dc0c150ec
Shashwat Srivastava <shashwat0805@protonmail.com>
2024-09-19 22:06:30 +0530

    changed url

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from io import BytesIO
 from urllib.parse import urlparse
 
 # flask --app api.py run --port=5000
-prediction_endpoint = f"http://127.0.0.1:5000/predict"
+prediction_endpoint = f"https://shashwat.lookhype.com/predict"
 
 st.title("Text Sentiment Predictor")
 
